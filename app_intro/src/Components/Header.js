5f3d4ed1b82014b54ef5cfd6b58677e7154cb28d
jokatty <jyotikattani@gmail.com>
2023-04-03 20:54:30 +0200

    Add react logo

--- app_intro/src/Components/Header.js
+++ app_intro/src/Components/Header.js
@@ -1,5 +1,10 @@
+import reactLogo from '../images/react-logo.png';
+
 export default function Header(){
   return(
-    <h1>I'm a simple header.</h1>
+    <>
+      <h1>I'm a simple header.</h1>
+      <img src={reactLogo} alt="react logo"/>
+    </>
   )
 }
