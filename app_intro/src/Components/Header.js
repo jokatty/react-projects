7a4301231ad8d72119856906fa9e5a0e2cfe741b
jokatty <jyotikattani@gmail.com>
2023-04-03 20:29:44 +0200

    Create App component and render in root

---
+++ app_intro/src/Components/Header.js
@@ -0,0 +1,5 @@
+export default function Header(){
+  return(
+    <h1>I'm a simple header.</h1>
+  )
+}
---
+++ app_intro/src/Components/Main.js
@@ -0,0 +1,11 @@
+export default function Main(){
+  return(
+    <div>
+      <h3>List of something: </h3>
+      <ul>
+        <li>something</li>
+        <li>something else</li>
+      </ul>
+    </div>
+  )
+}
